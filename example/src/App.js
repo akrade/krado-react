c54debf0875fb17075ab77c6fe081ee7663216d7
SetyMedia <setymedia@gmail.com>
2021-02-01 21:59:21 -0500

    Added basic "Container" component and created mobile layout.

--- example/src/App.js
+++ example/src/App.js
@@ -1,44 +1,46 @@
 import React from 'react';
-import { Flex, Grid, Text, Heading, Title } from 'krado-react';
+import { Flex, Grid, Text, Heading, Title, Container } from 'krado-react';
 
 export default function App() {
   return (
-    <Grid gridGap={4} gridTemplateColumns={`repeat( 3, minmax(1rem, 1fr) )`}>
-      <Flex
-        backgroundColor="muted"
-        justifyContent="center"
-        alignItems="center"
-        padding={5}
-        borderRadius={3}
+    <Container>
+      <Grid
+        gridGap={4}
+        gridTemplateColumns={['1fr', 'repeat( 3, minmax(1rem, 1fr) )']}
       >
-        <Text marginTop={5}>Text</Text>
-      </Flex>
-      <Flex
-        justifyContent="center"
-        flexDirection="column"
-        backgroundColor="muted"
-        padding={5}
-        borderRadius={3}
-      >
-        <Heading level={1}>Heading 1</Heading>
-        <Heading level={2}>Heading 2</Heading>
-        <Heading level={3}>Heading 3</Heading>
-        <Heading level={4}>Heading 4</Heading>
-        <Heading level={5}>Heading 5</Heading>
-        <Heading level={6}>Heading 6</Heading>
-      </Flex>
-      <Flex
-        padding={0}
-        justifyContent="center"
-        alignItems="center"
-        borderRadius={3}
-        backgroundColor="muted"
-      >
-        <Title>Title</Title>
-      </Flex>
-      <Title variant="display" color="primary" margin={6}>
-        Display
-      </Title>
-    </Grid>
+        <Flex
+          backgroundColor="muted"
+          justifyContent="center"
+          alignItems="center"
+          padding={5}
+          borderRadius={3}
+        >
+          <Text>Text</Text>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          flexDirection="column"
+          backgroundColor="muted"
+          padding={5}
+          borderRadius={3}
+        >
+          <Heading level={1}>Heading 1</Heading>
+          <Heading level={2}>Heading 2</Heading>
+          <Heading level={3}>Heading 3</Heading>
+          <Heading level={4}>Heading 4</Heading>
+          <Heading level={5}>Heading 5</Heading>
+          <Heading level={6}>Heading 6</Heading>
+        </Flex>
+        <Flex
+          padding={5}
+          justifyContent="center"
+          alignItems="center"
+          borderRadius={3}
+          backgroundColor="muted"
+        >
+          <Title>Title</Title>
+        </Flex>
+      </Grid>
+    </Container>
   );
 }
