dfbea15ddf128e3bafeaaf674b6bbec3aefddab3
SetyMedia <setymedia@gmail.com>
2021-04-19 21:35:42 -0400

    Add Box in example page to test color.js import

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex } from 'krado-react';
+import { Box, Flex, colors } from 'krado-react';
 import ComponentRow from './components/ComponentRow';
 import TextExample from './components/TextExample';
 import HeadingExample from './components/HeadingExample';
@@ -13,6 +13,7 @@ import TagExample from './components/TagExample';
 export default function App() {
   return (
     <Box variant="container" py={5}>
+      <Box backgroundColor={colors.yellow[2]} width="100px" height="100px" />
       <Flex flexDirection="column">
         <ComponentRow>
           <TextExample />
