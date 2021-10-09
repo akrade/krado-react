bc2303d608d472e3537d28c05d0f6635ae98a89a
Imsety Taylor <imsety@imsety.com>
2021-10-08 20:07:26 -0400

    Remove references to styled-components (KRAD-122)

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'krado-react';
 // import './styles/index.css';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
