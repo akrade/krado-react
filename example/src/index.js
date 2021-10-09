af1503244681a59816f655b82dc44be73d638874
Imsety Taylor <imsety@imsety.com>
2021-10-08 20:22:00 -0400

    Wrap example `App` with Theme UI `ThemeProvider` (KRAD-122)

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'theme-ui';
+import { theme } from 'krado-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
