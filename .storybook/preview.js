2a515694d65ab1ba102ca74a4a9f3547d466fb18
Imsety Taylor <imsety@imsety.com>
2021-10-18 16:21:23 -0400

    Wrap Storybook with Theme UI `ThemeProvider` (KRAD-153)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from '../src/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  )
+];
