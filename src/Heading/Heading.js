8f58c9e611e219c6e06ddfa56029e903c5a02f1e
Imsety Taylor <imsety@imsety.com>
2021-10-19 17:18:29 -0400

    Add `propTypes` to `Heading` (KRAD-153)

--- src/Heading/Heading.js
+++ src/Heading/Heading.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Heading as ThemeUiHeading } from 'theme-ui';
+import PropTypes from 'prop-types';
 
 export default function Heading({ ...rest }) {
   return <ThemeUiHeading {...rest} />;
 }
+
+Heading.propTypes = {
+  variant: PropTypes.oneOf([
+    'body.h1',
+    'body.h2',
+    'body.h3',
+    'body.h4',
+    'body.h5',
+    'display.h1',
+    'display.h2',
+    'display.h3',
+    'display.h4',
+    'display.h5'
+  ])
+};
 
 Heading.defaultProps = {
   variant: 'display.h2'
