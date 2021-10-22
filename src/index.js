5a63b1b5cc636d2f1f1c81c7ec414834ccf19f2d
Imsety Taylor <imsety@imsety.com>
2021-10-22 16:44:22 -0400

    Export `ToggleIcon`

--- src/index.js
+++ src/index.js
@@ -14,3 +14,4 @@ export { default as Card } from './Card/Card';
 export { default as Image } from './Image/Image';
 export { default as Avatar } from './Avatar/Avatar';
 export { default as Button } from './Button/Button';
+export { default as ToggleIcon } from './ToggleIcon/ToggleIcon';
