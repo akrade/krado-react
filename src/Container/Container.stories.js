6ceaf69f41736f6c8a3c288ce365ffc5a59b71df
Imsety Taylor <imsety@imsety.com>
2021-12-22 16:36:41 -0500

    Add `Container` cell extended from Theme UI's (KRAD-218)

---
+++ src/Container/Container.stories.js
@@ -0,0 +1,10 @@
+import Container from './Container';
+
+export default {
+  title: 'Container',
+  component: Container
+};
+
+export const Default = () => (
+  <Container backgroundColor="muted">Centered container</Container>
+);
