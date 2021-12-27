459acc00b722db6861e76615cd20d75558cc8bd9
Imsety Taylor <imsety@imsety.com>
2021-12-27 16:12:21 -0500

    Fix wrong `Container` name

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,5 +1,5 @@
 import { Container as ThemeUiContainer } from 'theme-ui';
 
-export default function Box({ ...rest }) {
+export default function Container({ ...rest }) {
   return <ThemeUiContainer paddingX={[3, 4, 5]} {...rest} />;
 }
